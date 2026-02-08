e8f4fb5b88bed8953f12d6bd6dc478bfc7ee99a0
Papercut Truths <mindmingle794@gmail.com>
2026-02-09 00:56:32 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'upscaler': path.resolve(__dirname, './node_modules/upscaler/dist/browser/esm/upscalerjs/src/browser/esm/index.js'),
+      '@upscalerjs/esrgan-slim': path.resolve(__dirname, './node_modules/@upscalerjs/esrgan-slim/dist/esm/models/esrgan-slim/src/index.js')
+    }
+  },
   build: {
     minify: 'terser',
     cssCodeSplit: true,
@@ -16,5 +27,6 @@ export default defineConfig({
         }
       }
     }
+
   }
 })
